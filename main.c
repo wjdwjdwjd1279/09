b0ecc824c3383fe8358a98bfc0a7fa9d6b1bbc9f
wjdwjdwjd1279 <alfzltm9721@naver.com>
2023-11-05 23:25:09 +0900

    9-2

--- main.c
+++ main.c
@@ -8,11 +8,13 @@ int main(int argc, char *argv[]) {
 	int grade[N_STUDENT];
 	int i;
 	
+	printf("input 5 scores:");
+		
 	for(i=0;i<N_STUDENT;i++)
-		grade[i] = (i+1)*10;
+		scanf("%d", &grade[i]);
 	
 	for(i=0;i<N_STUDENT;i++)
-		printf("grade[%i] = %i\n",i,grade[i]);
+		printf("grade[%d] = %d\n",i,grade[i]);
 	
 	
 	return 0;
